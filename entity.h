b8728a279b0c501d7cc3c0e7b850a12f0f0bf974
williamsonlogan <williamson.logan@gmail.com>
2018-03-22 18:59:53 -0700

    work on entity, started image wrapper
    Going to make wrappers for all of AGK, really streamline this thing

--- entity.h
+++ entity.h
@@ -1,14 +1,34 @@
 #pragma once
+
+#include "agk.h"
+#include <iostream>
+
 class Entity
 {
 public:
-	Entity();
+	Entity(float posx, float posy, std::string texturePath);
 	~Entity();
 
+	void Update();
+	void Render();
+
+	std::string getTag() { return _tag; }
+	int getID() { return _id; }
+	int getTexture() { return _texture; }
+	float getPosX() { return _posx; }
+	float getPosY() { return _posy; }
+
+	void setTag(std::string tag) { _tag = tag; }
+	void setID(int id) { _id = id; }
+	void setTexture(int texture) { _texture = texture; }
+	void setTexture(std::string texturePath); //TODO: Make this function.
+	void setPosition(float posx, float posy) { _posx = posx; _posy = posy; }
+
 private:
-	int id;
-	int image;
-	float posx;
-	float posy;
+	std::string _tag;
+	int _id;
+	int _texture;
+	float _posx;
+	float _posy;
 };
 
