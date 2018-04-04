60b3967d597dcf153972e18f343160dcdb57bd99
Logan <williamson.logan@gmail.com>
2018-04-03 18:32:46 -0700

    Work on that collision homie

--- macros.h
+++ macros.h
@@ -3,4 +3,4 @@
 #define RADS 3.14159 / 180
 #define MIN(a,b) (((a)<(b))?(a):(b))
 #define MAX(a,b) (((a)>(b))?(a):(b))
-#define CLAMP(x, upper, lower) (MIN(upper, MAX(x, lower)))
+#define CLAMP(x, upper, lower) (MIN(upper, MAX(x, lowerw)))
